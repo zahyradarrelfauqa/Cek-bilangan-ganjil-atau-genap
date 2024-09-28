01d691f66c4c76ea4e1b65b74d40f5adc020ad42
Zahyra Darrel Fauqa <2400018129@webmail.uad.ac.id>
2024-09-28 11:25:28 +0700

    Add files via upload

---
+++ Proyek 1_Cek bilangan ganjil atau genap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int cekGanjilGenap(int a){
+int hasilCek;
+
+if (a%2==0) {
+hasilCek=0;
+}else{
+hasilCek=1;
+}
+return hasilCek;
+}
+
+int main(){
+	
+int bilangan,cek;
+char hasil[10];
+
+printf("Masukkan Bilangan = "); scanf("%d", &bilangan);
+}
