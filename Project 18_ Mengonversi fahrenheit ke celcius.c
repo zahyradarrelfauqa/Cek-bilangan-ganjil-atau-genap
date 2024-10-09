35930994629f5ea243c3f72c817bf60679ebc69d
Zahyra Darrel Fauqa <2400018129@webmail.uad.ac.id>
2024-10-09 15:44:35 +0700

    Add files via upload

---
+++ Project 18_ Mengonversi fahrenheit ke celcius.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Fuction to convert Degree
+// Fahrenheit to Degree celsius
+
+float fahrenheit_to_celcius(float f);
+int main(){
+	return ((f-32.0)*5.0/9.0);
+	
+}
+
+int main ()
+{
+	float f = 40;
+	
+	// Passing parameter to function
+	printf("Temperature in Degree Celsius : %0.2f");
+	      fahrenheit_to_celcius (f));
+	return 0;
+}
