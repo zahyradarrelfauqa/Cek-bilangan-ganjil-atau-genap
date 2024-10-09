70e6ce543a9e34d99445be263cf21471c287ea13
Zahyra Darrel Fauqa <2400018129@webmail.uad.ac.id>
2024-10-09 15:43:50 +0700

    Add files via upload

---
+++ Project 17_ Kartu tanda mahasiswa.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	cout <<( "=========================================")<<endl;
+	cout <<( "=\tKARTU TANDA MAHASISWA\t\t=")<<endl;
+	cout <<( "=  \tUNIVERSITAS AHMAD DAHLAN \t=\t")<<endl;
+	cout << ( "=========================================")<<endl;
+	cout << ( "=\t\t\t\t\t=")<<endl;
+	cout << ("=\tNama : Zahyra Darrel Fauqa\t=")<<endl;
+	cout << ("=\tNIM : 2400018129\t\t=")<<endl;
+	cout << ("=\tProgram Studi : Informatika\t=")<<endl;
+	cout << ("=\tFakultas : Teknologi Industri\t=")<<endl;
+	cout << ("=\t\t\t\t\t=")<<endl;
+	cout << ("=\t\t\t\t\t=")<<endl;
+	cout << ("=========================================");
+	
+	return 0;
+}
